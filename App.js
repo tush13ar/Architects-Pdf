3d4ee5e46b61f7f0fe3bb685db988f6ad32251ad
Tushar <tush13ar@gmail.com>
2021-05-18 17:44:30 +0530

    second commit

--- App.js
+++ App.js
@@ -1,26 +1,25 @@
-import 'react-native-gesture-handler'
+import 'react-native-gesture-handler';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native'
-import {createStackNavigator} from '@react-navigation/stack'
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-import PDF from './pdf'
-import PdfList from './pdfList'
+import PDF from './src/screens/pdf';
+import PdfList from './src/screens/pdfList';
 
-const Stack = createStackNavigator() 
+const Stack = createStackNavigator();
 
-//Stack Navigator containing Pdf List Screen (consisting of list of PDF's uploaded to the App) 
-//and Pdf Screen (displaying individual PDF) 
+//Stack Navigator containing Pdf List Screen (consisting of list of PDF's uploaded to the App)
+//and Pdf Screen (displaying individual PDF)
 
 const App = () => {
-  return(
-      <NavigationContainer>
-        <Stack.Navigator>
-        <Stack.Screen name='PdfList' component={PdfList}/>
-        <Stack.Screen name='Pdf' component={PDF} />
-        </Stack.Navigator>
-      </NavigationContainer>
-
-  )
-}
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="PdfList" component={PdfList} />
+        <Stack.Screen name="Pdf" component={PDF} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;
